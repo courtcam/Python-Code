132c56de12632e01f59216835bac3bd42738ad55
Courtney Campbell <courtcam1995@gmail.com>
2023-02-13 03:36:25 +0000

    final commit

--- ListOfDictionaries.py
+++ ListOfDictionaries.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3.7
-
 # generates a list of dictionaries about files in the working directory
+
 import os
 
 # Get working directory and assign it to the varible "Path" 
 path = os.getcwd()
 
 #list all the DIR in the current working DIR by passing in the path parameter
-ListOfDir  = os.listdir(path)
+ListOfFiles  = os.listdir(path)
 
-for i in ListOfDir:
+# A for loop that iterate over the list of files 
+for i in ListOfFiles:
  
-   pathOfFile = path + "/"+ i 
+# cantatinating the current working directory and each file to get the path and assign it to a variable
+   pathOfFile = path + "/"+ i
+# get the size of the path and assign it to a variable
    size = os.path.getsize(path + "/"+ i)
    ListOfDict = {'NAME' : i, 'PATH' : pathOfFile, 'SIZE' : size}
    print(ListOfDict)
